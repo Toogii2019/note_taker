7c964b65a9e1d40a2bce1bd295e0e6874ee854e1
Toogii <dashdava@uw.edu>
2020-08-31 00:28:37 -0700

    FIxing typo

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on port ${PportRT}`));
+app.listen(port, () => console.log(`Listening on port ${port}`));
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/index.html"));
